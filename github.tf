85002cf96e91c3be8429ed46d80677910f9b83e5
Tobias Wiesenthal <tobias@wiesenthal.biz>
2017-04-14 01:58:52 +0200

    split github and aws resources

--- github.tf
+++ github.tf
@@ -1,17 +1,4 @@
 variable "github_token" {}
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
-
-variable "aws_default_region" {
-  default = "eu-central-1"
-}
-
-# Configure the AWS provider
-provider "aws" {
-  access_key = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
-  region     = "${var.aws_default_region}"
-}
 
 # Configure the GitHub Provider
 provider "github" {
