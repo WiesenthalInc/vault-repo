196506dd4719cf68c67a35c9876bd2636cdf9811
Tobias Wiesenthal <tobias@wiesenthal.biz>
2017-04-14 12:39:54 +0200

    added separate file for variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# AWS provider variables
+variable "aws_access_key_id" {}
+
+variable "aws_secret_access_key" {}
+
+variable "aws_default_region" {
+  default = "eu-central-1"
+}
+
+# Github provider variables
+variable "github_token" {}
